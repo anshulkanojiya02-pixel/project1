626aa454a6c7420ff24e5246e762fabc7e1f1fa4
anshul <anshul.kanojiya02@gmail.com>
2026-02-07 15:16:17 +0530

    first

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
             features = "src/test/java/features/",
             glue = {"stepdefinition", "utility" },
-            plugin={"pretty", "html:target/cucumber-reports/report.html","json:cucumber.json"}
+            plugin={"pretty", "html:target/cucumber-reports/report.html", "json:target/cucumber-reports/cucumber.json", "junit:target/cucumber-reports/cucumber.xml" }
     )
     public class TestRunner {
 }
